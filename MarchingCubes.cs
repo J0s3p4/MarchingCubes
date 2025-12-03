f4a8e0c3e82a67047dfbe476f3353ddffe934693
Joseph Carlyle <joseph.j.carlyle@gmail.com>
2025-12-03 15:46:13 +0000

    Setup

--- MarchingCubes.cs
+++ MarchingCubes.cs
@@ -1,9 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+// Attach Script to a game object with Components: Mesh Filter, Mesh Renderer
 public class MarchingCubes : MonoBehaviour
 {
+
+    List<Vector3> vertices = new List<Vector3>();
+    List<int> triangles = new List<int>();
+
+    MeshFilter meshFilter;
+
+    private void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+    }
+
+    void ClearMeshData()
+    {
+        vertices.Clear();
+        triangles.Clear();
+    }
+
+    void BuildMesh()
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
+        meshFilter.mesh = mesh;
+    }
+
+
 
 }
